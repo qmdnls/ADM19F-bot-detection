f753c9d956264163588dcef989da4ab12d911eed
Björn Bebensee <bebensee@snu.ac.kr>
2019-10-12 22:23:08 +0900

    Added functionality to retrieve username, # follower, # following data using Twitter API

--- get_usernames.py
+++ get_usernames.py
@@ -2,6 +2,12 @@ import pandas as pd
 import tweepy
 from itertools import zip_longest
 
+# Helper method that returns a list in chunks
+def grouper(iterable, n, fillvalue=None):
+    "grouper('ABCDEFG', '3', 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return zip_longest(fillvalue=fillvalue, *args)
+
 # Load Twitter API key
 # It's assumed our file contains the consumer token on the first line and its secret on the second line
 try:
@@ -17,23 +23,37 @@ except FileNotFoundError:
 # Set up tweepy
 auth = tweepy.AppAuthHandler(consumer_token, consumer_secret)
 api = tweepy.API(auth)
+api.wait_on_rate_limit = True
+api.wait_on_rate_limit_notify = True
 
 # Load dataset from tsv
 df = pd.read_csv("data-set-cresci-2018.tsv", sep="\t")
 
-# Create new empty column to store username in
-df["username"] = ""
-df["followers"] = ""
-df["following"] = ""
+# Create new dataframe to store retrieved data in
+users_df = pd.DataFrame()
+users_df["user_id"] = ""
+users_df["screen_name"] = ""
+users_df["followers"] = ""
+users_df["following"] = ""
 
 # Use Twitter API to get usernames, follower and following counts, save in Pandas dataframe
-for idx, id in enumerate(df["user_id"][0:5]):
+for user_ids in grouper(df["user_id"][0:100], 100):
+    # Group of user ids now looks like: (user_id0, user_id1, ..., user_id99)
+    
     try:
-        user = api.get_user(user_id=id)
-        print(user.screen_name)
-        print("Followers: " + str(user.followers_count))
-        print("Following: " + str(user.friends_count))
+        users = api.lookup_users(user_ids=user_ids)
+    
+    # Handle exception in case the user does not exist
     except tweepy.error.TweepError:
         print("User_ID not found: '%s'" % id)
+    
+    # Now iterate over the users and modify dataframe accordingly
+    for idx,user in enumerate(users):
+        users_df.at[idx, "user_id"] = user.id
+        users_df.at[idx, "screen_name"] = user.screen_name
+        users_df.at[idx, "followers"] = user.followers_count
+        users_df.at[idx, "following"] = user.friends_count
 
-print(api.rate_limit_status())
+df = pd.merge(df, users_df, on='user_id')
+df.to_csv("data.csv", index = None, header=True)
+print(df)
