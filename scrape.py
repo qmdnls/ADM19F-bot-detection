de20b97676f38d7b413526db195905049fc57cd5
Björn Bebensee <bebensee@snu.ac.kr>
2019-10-12 19:36:53 +0900

    Iterate over user IDs in dataset

--- scrape.py
+++ scrape.py
@@ -1,24 +1,30 @@
 import pandas as pd
 import numpy as np
 import twint
-import csv
 
 dataset_df = pd.read_csv("data-set-cresci-2018.tsv", sep="\t")
-print(dataset_df)
 
 c = twint.Config()
-c.Username = "1234a"
 c.Followers = True
 c.Pandas = True
 c.Hide_output = True
 
-twint.run.Followers(c)
-df = twint.storage.panda.Follow_df
-followers = df["followers"].tolist()[0]
 
-twint.run.Following(c)
-df = twint.storage.panda.Follow_df
-following = df["following"].tolist()[0]
+for id in dataset_df["user_id"]:
+    c.User_id = id
+    
+    twint.run.Followers(c)
 
-print(followers)
-print(following)
+    if (twint.storage.panda.Follow_df.empty):
+        continue
+
+    df = twint.storage.panda.Follow_df
+    followers = df["followers"].tolist()[0]
+
+    twint.run.Following(c)
+    df = twint.storage.panda.Follow_df
+    following = df["following"].tolist()[0]
+
+    print("User_id: " + str(id))
+    print(followers)
+    print(following)
