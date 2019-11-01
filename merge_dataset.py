e03b200c3f8af6eae86fd8cd3045a6211e51b6ba
Björn Bebensee <bebensee@snu.ac.kr>
2019-11-02 02:49:50 +0900

    Merge datasets

--- merge_dataset.py
+++ merge_dataset.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
+#af = pd.read_csv("data2.csv", encoding='utf8', engine='python')
+#bf = pd.read_csv("progress_final.csv", encoding='utf8', engine='python')
+#af['user_id'] = af['user_id'].astype(int)
+#df = pd.merge(af, bf, on='user_id')
+#print(df.head)
 
-af = pd.read_csv("data2.csv", encoding='utf8', engine='python')
-bf = pd.read_csv("progress_final.csv", encoding='utf8', engine='python')
-af['user_id'] = af['user_id'].astype(int)
-print(af.head)
-print(bf.head)
-df = pd.merge(af, bf, on='user_id')
-print(df.head)
+af = pd.read_csv("dataset.csv", encoding="utf8", engine="python")
+bf = pd.read_csv("corrected.csv", encoding="utf8", engine="python")
 
-df.to_csv("dataset.csv", index=None, header=True)
+df = pd.concat([af, bf])
+
+df.to_csv("users.csv", index=None, header=True)
