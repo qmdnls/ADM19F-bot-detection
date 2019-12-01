9f5b501b195345e68e029f159dbc39bde1c9faf9
nagmat <51871069+nagmat1@users.noreply.github.com>
2019-12-01 21:25:55 +0900

    pdf version of graphs
    pdf version of graphs

---
+++ new_graph/pdf/pdf/favourites_count.py
@@ -0,0 +1,21 @@
+import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np 
+
+df = pd.read_csv('train_graph.csv',encoding='utf8',engine='python',chunksize=None)
+
+human = df[df.label == 0]
+bot   = df[df.label == 1]
+
+plt.figure()
+plt.hist([human['status_successors'],bot['status_successors']],bins=50,density=False,range=(0,40000),color=["blue","red"],cumulative=False,label=["Human","Bot"],histtype='bar') 
+#plt.hist(bot['status_successors'],bins=250,density=False,range=(0,4000),color="red",cumulative=False,label="bots",histtype='bar') 
+plt.legend()
+plt.ylabel("frequency")
+plt.xlabel("status_successors")
+#plt.show()
+plt.savefig("status_successors.pdf"); 
+
+
